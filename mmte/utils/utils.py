856ceb985d3541d2a09e10f02ad39cd36a90acc9
jankinf <fzw5782@126.com>
2024-05-20 16:21:33 +0800

    support cfg-options to overwrite yaml config file

--- mmte/utils/utils.py
+++ mmte/utils/utils.py
@@ -1,6 +1,9 @@
-import os
+from argparse import Action, ArgumentParser, Namespace
+from typing import Any, Sequence, Union, Dict
 from mmte import lib_path
 import requests
+import copy
+import os
 
 def get_abs_path(rel):
     return os.path.join(lib_path, rel)
@@ -54,3 +57,133 @@ def get_task_type(task_id):
         raise ValueError(f"Unknown task_id suffix: {suffix}")
 
 
+
+class DictAction(Action):
+    """
+    argparse action to split an argument into KEY=VALUE form
+    on the first = and append to a dictionary. List options can
+    be passed as comma separated values, i.e 'KEY=V1,V2,V3', or with explicit
+    brackets, i.e. 'KEY=[V1,V2,V3]'. It also support nested brackets to build
+    list/tuple values. e.g. 'KEY=[(V1,V2),(V3,V4)]'
+    """
+
+    @staticmethod
+    def _parse_int_float_bool(val: str) -> Union[int, float, bool, Any]:
+        """parse int/float/bool value in the string."""
+        try:
+            return int(val)
+        except ValueError:
+            pass
+        try:
+            return float(val)
+        except ValueError:
+            pass
+        if val.lower() in ['true', 'false']:
+            return True if val.lower() == 'true' else False
+        if val == 'None':
+            return None
+        return val
+
+    @staticmethod
+    def _parse_iterable(val: str) -> Union[list, tuple, Any]:
+        """Parse iterable values in the string.
+
+        All elements inside '()' or '[]' are treated as iterable values.
+
+        Args:
+            val (str): Value string.
+
+        Returns:
+            list | tuple | Any: The expanded list or tuple from the string,
+            or single value if no iterable values are found.
+
+        Examples:
+            >>> DictAction._parse_iterable('1,2,3')
+            [1, 2, 3]
+            >>> DictAction._parse_iterable('[a, b, c]')
+            ['a', 'b', 'c']
+            >>> DictAction._parse_iterable('[(1, 2, 3), [a, b], c]')
+            [(1, 2, 3), ['a', 'b'], 'c']
+        """
+
+        def find_next_comma(string):
+            """Find the position of next comma in the string.
+
+            If no ',' is found in the string, return the string length. All
+            chars inside '()' and '[]' are treated as one element and thus ','
+            inside these brackets are ignored.
+            """
+            assert (string.count('(') == string.count(')')) and (
+                    string.count('[') == string.count(']')), \
+                f'Imbalanced brackets exist in {string}'
+            end = len(string)
+            for idx, char in enumerate(string):
+                pre = string[:idx]
+                # The string before this ',' is balanced
+                if ((char == ',') and (pre.count('(') == pre.count(')'))
+                        and (pre.count('[') == pre.count(']'))):
+                    end = idx
+                    break
+            return end
+
+        # Strip ' and " characters and replace whitespace.
+        val = val.strip('\'\"').replace(' ', '')
+        is_tuple = False
+        if val.startswith('(') and val.endswith(')'):
+            is_tuple = True
+            val = val[1:-1]
+        elif val.startswith('[') and val.endswith(']'):
+            val = val[1:-1]
+        elif ',' not in val:
+            # val is a single value
+            return DictAction._parse_int_float_bool(val)
+
+        values = []
+        while len(val) > 0:
+            comma_idx = find_next_comma(val)
+            element = DictAction._parse_iterable(val[:comma_idx])
+            values.append(element)
+            val = val[comma_idx + 1:]
+
+        if is_tuple:
+            return tuple(values)
+
+        return values
+
+    def __call__(self,
+                 parser: ArgumentParser,
+                 namespace: Namespace,
+                 values: Union[str, Sequence[Any], None],
+                 option_string: str = None):
+        """Parse Variables in string and add them into argparser.
+
+        Args:
+            parser (ArgumentParser): Argument parser.
+            namespace (Namespace): Argument namespace.
+            values (Union[str, Sequence[Any], None]): Argument string.
+            option_string (list[str], optional): Option string.
+                Defaults to None.
+        """
+        # Copied behavior from `argparse._ExtendAction`.
+        options = copy.copy(getattr(namespace, self.dest, None) or {})
+        if values is not None:
+            for kv in values:
+                key, val = kv.split('=', maxsplit=1)
+                options[key] = self._parse_iterable(val)
+        setattr(namespace, self.dest, options)
+
+
+def merge_config(dict1: Dict[str, Any], dict2: Dict[str, Any]) -> Dict[str, Any]:
+    dict1 = copy.deepcopy(dict1)
+    for keys, value in dict2.items():
+        key_list = keys.split('.')
+
+        cur_dict = dict1
+        for key_name in key_list[:-1]:
+            if not hasattr(cur_dict, key_name):
+                cur_dict[key_name] = {}
+            cur_dict = cur_dict[key_name]
+        
+        cur_dict[key_list[-1]] = value
+    
+    return dict1
--- run_task.py
+++ run_task.py
@@ -1,7 +1,25 @@
 from mmte.tasks.base import BaseTask
 from mmte.utils.registry import registry
 from mmte.evaluators.metrics import _supported_metrics
+from mmte.utils.utils import DictAction, merge_config
+import argparse
 import yaml
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='config file path')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
+    args = parser.parse_args()
+    return args
 
 print("models: ", registry.list_chatmodels())
 print("datasets: ", registry.list_datasets())
@@ -11,31 +29,21 @@ print("metrics: ", list(_supported_metrics.keys()))
 
 
 if __name__ == '__main__':
-    configs = [
-        # "mmte/configs/task/confaide_image.yaml",
-        # "mmte/configs/task/confaide_text_color.yaml",
-        # "mmte/configs/task/confaide_text.yaml",
-        # "mmte/configs/task/vispriv_recognition_vispr.yaml",
-        # "mmte/configs/task/vispriv_recognition_vizwiz.yaml",
-        "mmte/configs/task/visual_leakage_protected.yaml",
-        "mmte/configs/task/visual_leakage.yaml",
-    ]
+    args = parse_args()
+    config = args.config
 
-    for config in configs:
-        with open(config, 'r') as f:
-            cfg = yaml.load(f, Loader=yaml.FullLoader)
-            print(config)
-            print(cfg)
-
-            model_id = cfg.get('model_id')
-            dataset_id = cfg.get('dataset_id')
-            log_file = cfg.get('log_file')
-            method_cfg = cfg.get('method_cfg', {})
-            evaluator_seq_cfgs = cfg.get('evaluator_seq_cfgs', [])
-            
-            runner = BaseTask(dataset_id=dataset_id, model_id=model_id, method_cfg=method_cfg, log_file=log_file, evaluator_seq_cfgs=evaluator_seq_cfgs)
-            runner.pipeline()
-
-
-
-
+    with open(config, 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+        if hasattr(args, "cfg_options"):
+            cfg = merge_config(cfg, args.cfg_options)
+        print(config)
+        print(cfg)
+        model_id = cfg.get('model_id')
+        dataset_id = cfg.get('dataset_id')
+        log_file = cfg.get('log_file')
+        method_cfg = cfg.get('method_cfg', {})
+        dataset_cfg = cfg.get('dataset_cfg', {})
+        evaluator_seq_cfgs = cfg.get('evaluator_seq_cfgs', [])
+        
+        runner = BaseTask(dataset_id=dataset_id, model_id=model_id, method_cfg=method_cfg, dataset_cfg=dataset_cfg, log_file=log_file, evaluator_seq_cfgs=evaluator_seq_cfgs)
+        runner.pipeline()
