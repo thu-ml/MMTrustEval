f33361b63c4006f16ba45657d040919669b8b19b
jankinf <fzw5782@126.com>
2024-06-23 12:55:11 +0800

    robustness scripts update

--- scripts/run/robustness_scripts/r1-ood-artistic.sh
+++ scripts/run/robustness_scripts/r1-ood-artistic.sh
@@ -10,5 +10,5 @@ categories=(
 
 for category in "${categories[@]}";
 do
-    CUDA_VISIBLE_DEVICES=1 python run_task.py --config mmte/configs/task/robustness/ood-artistic.yaml --cfg-options dataset_id="coco-o-${category}" log_file="logs/robustness/ood-artistic-${category}.json"
+    CUDA_VISIBLE_DEVICES=1 python run_task.py --config mmte/configs/task/robustness/r1-ood-artistic.yaml --cfg-options dataset_id="coco-o-${category}" log_file="logs/robustness/ood-artistic-${category}.json"
 done
--- scripts/run/robustness_scripts/r6-adversarial-text.sh
+++ scripts/run/robustness_scripts/r6-adversarial-text.sh
@@ -6,5 +6,5 @@ dataset_ids=(
 
 for dataset_id in "${dataset_ids[@]}";
 do
-    CUDA_VISIBLE_DEVICES=1 python run_task.py --config mmte/configs/task/robustness/adversarial-text.yaml --cfg-options dataset_id=${dataset_id} log_file="logs/robustness/adversarial-text-${dataset_id}.json"
+    CUDA_VISIBLE_DEVICES=1 python run_task.py --config mmte/configs/task/robustness/r6-adversarial-text.yaml --cfg-options dataset_id=${dataset_id} log_file="logs/robustness/adversarial-text-${dataset_id}.json"
 done
