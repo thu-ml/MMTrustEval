a18fb7b62a75c9779700c1da4b1aca35974101e9
jankinf <fzw5782@126.com>
2024-11-04 11:09:31 +0800

    bug fixed: json dump

--- mmte/tasks/base.py
+++ mmte/tasks/base.py
@@ -6,12 +6,23 @@ from mmte.utils.registry import registry
 from mmte.methods.base import BaseMethod
 from mmte.models.base import BaseChat
 from mmte.evaluators.base import SequentialEvaluator
+import numpy as np
 import warnings
 import json
 import os
 
-class BaseTask(ABC):    
-    def __init__(self, dataset_id: str, model_id: str, method_cfg: Optional[Dict] = {}, dataset_cfg: Optional[Dict] = {}, generation_kwargs: Optional[Dict] = {}, evaluator_seq_cfgs: List = [], log_file: Optional[str] = None) -> None:
+
+class BaseTask(ABC):
+    def __init__(
+        self,
+        dataset_id: str,
+        model_id: str,
+        method_cfg: Optional[Dict] = {},
+        dataset_cfg: Optional[Dict] = {},
+        generation_kwargs: Optional[Dict] = {},
+        evaluator_seq_cfgs: List = [],
+        log_file: Optional[str] = None,
+    ) -> None:
         self.dataset_id = dataset_id
         self.model_id = model_id
         self.method_cfg = method_cfg
@@ -19,29 +30,29 @@ class BaseTask(ABC):
         self.evaluator_seq_cfgs = evaluator_seq_cfgs
         self.generation_kwargs = generation_kwargs
         self.log_file = log_file
-    
+
     def get_handlers(self) -> None:
         self.evaluators = self.get_evaluators()
-        self.method = self.get_method() # get method before dataset
+        self.method = self.get_method()  # get method before dataset
         self.dataset = self.get_dataset()
         self.model = self.get_model()
-    
+
     def get_model(self) -> BaseChat:
         model_cls = registry.get_chatmodel_class(self.model_id)
         model = model_cls(self.model_id)
         return model
-    
+
     def get_method(self) -> BaseMethod:
         if not self.method_cfg:
             return None
-        
+
         assert len(self.method_cfg.keys()) == 1
         method_id = list(self.method_cfg.keys())[0]
         method_kwargs = self.method_cfg[method_id]
         method_cls = registry.get_method_class(method_id)
         method = method_cls(method_id, **method_kwargs)
         return method
-    
+
     def get_evaluators(self) -> List[SequentialEvaluator]:
         evaluators = []
 
@@ -51,18 +62,24 @@ class BaseTask(ABC):
 
     def get_dataset(self) -> BaseDataset:
         dataset_cls: Type[BaseDataset] = registry.get_dataset_class(self.dataset_id)
-        dataset = dataset_cls(self.dataset_id, method_hook=self.method, **self.dataset_cfg)
+        dataset = dataset_cls(
+            self.dataset_id, method_hook=self.method, **self.dataset_cfg
+        )
         return dataset
-    
+
     def get_dataloader(self) -> DataLoader:
-        dataloader = DataLoader(dataset=self.dataset, batch_size=1, collate_fn=collate_fn)
+        dataloader = DataLoader(
+            dataset=self.dataset, batch_size=1, collate_fn=collate_fn
+        )
         return dataloader
 
-    def eval(self, responses: List[Dict[str, Any]]) -> Dict[str, Union[float, Sequence]]:
-        contents: Sequence[str] = [response['content'] for response in responses]
-        preds: Sequence[str] = [response['response'] for response in responses]
-        labels: Sequence[str] = [response['target'] for response in responses]
-        extras: Sequence[str] = [response['extra'] for response in responses]
+    def eval(
+        self, responses: List[Dict[str, Any]]
+    ) -> Dict[str, Union[float, Sequence]]:
+        contents: Sequence[str] = [response["content"] for response in responses]
+        preds: Sequence[str] = [response["response"] for response in responses]
+        labels: Sequence[str] = [response["target"] for response in responses]
+        extras: Sequence[str] = [response["extra"] for response in responses]
         results = {}
 
         for evaluator in self.evaluators:
@@ -70,24 +87,32 @@ class BaseTask(ABC):
             for key in result.keys():
                 if key in results.keys():
                     warnings.warn(f"{key} already exists in results.")
-            results.update(result)            
+            results.update(result)
 
-        #sub aspects
+        # sub aspects
         subset_eval = extras[0] is not None and "subset" in extras[0]
         if subset_eval:
             # Evaluate with subset of dataset, `extra` field in dataclass must have `subset` key to enable subset evaluation.
-            subset_list = [item['subset'] for item in extras]
+            subset_list = [item["subset"] for item in extras]
             assert any(subset_list)
 
             subsets = set(subset_list)
             for subset in subsets:
-                preds_subset = [preds[i] for i in range(len(preds)) if subset_list[i] == subset]
-                labels_subset = [labels[i] for i in range(len(labels)) if subset_list[i] == subset]
-                extras_subset = [extras[i] for i in range(len(extras)) if subset_list[i] == subset]
+                preds_subset = [
+                    preds[i] for i in range(len(preds)) if subset_list[i] == subset
+                ]
+                labels_subset = [
+                    labels[i] for i in range(len(labels)) if subset_list[i] == subset
+                ]
+                extras_subset = [
+                    extras[i] for i in range(len(extras)) if subset_list[i] == subset
+                ]
 
                 subset_results = {}
                 for evaluator in self.evaluators:
-                    subset_result = evaluator(preds_subset, labels_subset, extras_subset)
+                    subset_result = evaluator(
+                        preds_subset, labels_subset, extras_subset
+                    )
                     for key in subset_result.keys():
                         subset_key = f"{key}_{subset}"
                         if subset_key in results.keys():
@@ -99,23 +124,29 @@ class BaseTask(ABC):
 
         results.update(
             {
-                'content': contents if any(contents) else None,
-                'pred': preds if any(preds) else None,
-                'label': labels if any(labels) else None,
-                'extra': extras if any(extras) else None,
+                "content": contents if any(contents) else None,
+                "pred": preds if any(preds) else None,
+                "label": labels if any(labels) else None,
+                "extra": extras if any(extras) else None,
             }
         )
         return results
 
     def save_results(self, results: Dict[str, Any]) -> None:
-        scatter_keys = [key for key, value in results.items() if isinstance(value, Sequence)]
-        summary_keys = [key for key, value in results.items() if not isinstance(value, Sequence) and value is not None]
-        
+        scatter_keys = [
+            key for key, value in results.items() if isinstance(value, Sequence)
+        ]
+        summary_keys = [
+            key
+            for key, value in results.items()
+            if not isinstance(value, Sequence) and value is not None
+        ]
+
         if scatter_keys:
             seq_len = len(results[scatter_keys[0]])
             for key in scatter_keys:
                 assert len(results[key]) == seq_len
-            
+
         per_sample_results = []
         for idx in range(seq_len):
             per_sample_result = {}
@@ -124,11 +155,15 @@ class BaseTask(ABC):
             per_sample_results.append(per_sample_result)
 
         formatted_results = {}
-        formatted_results['total_results'] = {}
-        formatted_results['per_sample_results'] = per_sample_results
+        formatted_results["total_results"] = {}
+        formatted_results["per_sample_results"] = per_sample_results
         for key in summary_keys:
-            formatted_results['total_results'][key] = results[key]
-        
+            formatted_results["total_results"][key] = (
+                float(results[key])
+                if isinstance(results[key], (np.floating, np.integer))
+                else results[key]
+            )
+
         if self.log_file is not None:
             # check if the folder exists
             if not os.path.exists(os.path.dirname(self.log_file)):
@@ -137,49 +172,51 @@ class BaseTask(ABC):
             with open(self.log_file, "w") as f:
                 json.dump(formatted_results, f, indent=4)
 
-    def generate(self, dataloader: DataLoader, **generate_kwargs) -> List[Dict[str, Any]]:
-        print('len(self.dataset): ', len(dataloader.dataset))
+    def generate(
+        self, dataloader: DataLoader, **generate_kwargs
+    ) -> List[Dict[str, Any]]:
+        print("len(self.dataset): ", len(dataloader.dataset))
         responses = []
         for batch_data in dataloader:
             for data in batch_data:
                 """
-                    # for text data
-                    message = [
-                        {
-                            "role": "user",
-                            "content": text
-                        }
-                    ]
+                # for text data
+                message = [
+                    {
+                        "role": "user",
+                        "content": text
+                    }
+                ]
 
-                    # for multimodal data
-                    message = [
-                        {
-                            "role": "user",
-                            "content": {
-                                "image_path": ...,
-                                "text": ...
-                            }
+                # for multimodal data
+                message = [
+                    {
+                        "role": "user",
+                        "content": {
+                            "image_path": ...,
+                            "text": ...
                         }
-                    ]
+                    }
+                ]
                 """
-                
-                message = data['message']
-                target = data['target']
-                extra: Dict[str, Any] = data['extra']
+
+                message = data["message"]
+                target = data["target"]
+                extra: Dict[str, Any] = data["extra"]
 
                 response = self.model.chat(messages=message, **generate_kwargs)
                 output = {
-                    "content": message[0]['content'],
+                    "content": message[0]["content"],
                     "response": response.content,
                     "target": target,
                     "extra": extra,
                 }
-                print("output:",output)
-            
+                print("output:", output)
+
                 responses.append(output)
-        
+
         return responses
-        
+
     def pipeline(self) -> None:
         self.get_handlers()
         dataloader = self.get_dataloader()
